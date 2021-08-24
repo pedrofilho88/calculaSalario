7eb89ea5ebfc4e49a3bfecf97627d255521a3246
pedrofilho88 <pedrofilhooliveirasantos@gmail.com>
2021-08-23 22:53:36 -0300

    feita implementaçao de horas extras

--- biblio.js
+++ biblio.js
@@ -5,16 +5,53 @@ function transformaCampo ( idCampo ) {
    valor = parseInt(valor.replace(/[\D]+/g, ''));
    valor = valor + '';
    valor = valor.replace(/([0-9]{2})$/g, ",$1");
-    if (valor.length > 6) valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    elemento.value = valor;
-    if(valor == 'NaN') elemento.value = '';
-    sBase = converteFloat(valor);
+   if (valor.length > 6) valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+   elemento.value = valor;
+   if(valor == 'NaN') elemento.value = '';
+   sBase = converteFloat(valor);       
 }
 
+
+function transformaBRL (vlr){
+   vlr = parseInt(vlr.replace(/[\D]+/g, ''));
+   vlr = vlr + '';
+   vlr = vlr.replace(/([0-9]{2})$/g, ",$1");
+   if (vlr.length > 6) vlr = vlr.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+   if(vlr == 'NaN') vlr = '';
+   return vlr;
+}
+
+// Converte para numero (ex: 10000.21)
 function converteFloat(vString){
     let vlFloat = vString.replace(/[.]/g,'');
     vlFloat = vlFloat.replace(/,/g,'.');
     if(vlFloat == 'NaN') vlFloat = '';
-    parseFloat(vlFloat);
+    vlFloat = parseFloat(vlFloat);
     return vlFloat;
 }
+
+function mudaAtributo (id, param){
+    document.getElementById(id).setAttribute("style", param);
+}
+
+function colocaSombraBotao () {
+    document.getElementById("botao").setAttribute("style", "box-shadow: 5px 5px 5px black;");
+}
+
+function tiraSobmraBotao (){
+    document.getElementById("botao").setAttribute("style", "box-shadow: 0px 0px 0px black;");
+    setInterval(colocaSombraBotao, 600);
+    
+}
+
+
+
+
+// function troca(){
+// setInterval(document.getElementsByClassName('ui-datepicker-close').setAttribute(name, "Feito"), 30);
+// }
+
+// function trocaNome(){
+// troca();
+// }
+
--- server.js
+++ server.js
@@ -1,91 +1,150 @@
-    let vlInss;
-    let vlIrrf;
-    let hrCem;
-    let hrCinquenta;
-    let hrSobreaviso;
-    let dsr;
+    var vlInss;
+    var vlIrrf;
+    var hr100;
+    var hr50;
+    var hrsa;
+    var hrTotal;
+    var dsr;
     var sBase;
-    let sLiquido ;
+    var sBruto;
+    var sLiquido ;
     var percentInss;
     var percentIrrf;
     var percentLiquido;
     var percentDescontos;
+    var faixaInss;
+    var faixaIrrf;
+    var fgtsMes;
+    var totalDescontos;
 
-
-
-
-    
 
     function calculaSalarioLiquido(){
-        sLiquido = (sBase - vlInss) - vlIrrf;
+        sLiquido = (sBruto - vlInss) - vlIrrf;
+          
     }
 
     function calculaDsr(qtdHora, diasUteis, dmFeriados){
         dsr = ((calculaValorHora(sBase) * qtdHora) / diasUteis) * dmFeriados;
     }
 
-    function calculaValorHora(salario) {
-        return salario / 220;
+    function calculaHora50() {
+        let qtHoras = document.getElementById("hr50").value;
+        parseFloat(sBase);        
+        hr50 = ((sBase / 220) * qtHoras) + (((sBase / 220) * qtHoras)  * 0.5);
     }
 
-    function calculaPlantao(qtHoras, multiplicador) {
-        return qtHoras * multiplicador;
+    function calculaHora100() {
+        let qtHoras = document.getElementById("hr100").value;
+        hr100 = ((sBase / 220) * qtHoras) * 2 ;
+        parseFloat(hr100);
     }
+
+    function calculaHoraSa() {
+        let qtHoras = document.getElementById("hrsa").value;
+        hrsa = ((sBase / 220) * qtHoras) / 3 ;
+        parseFloat(hrsa);
+        
+    }
+    
+    function calculaHorasTotal() {
+        hrTotal = (hr50 + hr100) + hrsa;
+        
+              
+    }
+
     function calculaInss(totalRecebido) {        
         if (totalRecebido > 6433.57) {
             vlInss = 751.99
+            faixaInss = "Teto (R$ 751,99)";
         } 
         else if (totalRecebido > 3305.22) {
             vlInss = ((totalRecebido - 3305.23) * 0.14) + 132.20 + 99.31 + 82.50;
+            faixaInss = "5ª faixa (14%)";
         } 
         else if (totalRecebido > 2203.48) {
             vlInss = ((totalRecebido -2203.49 ) * 0.12) + 99.31 + 82.50;
+            faixaInss = "3ª faixa (12%)";
         } 
         else if (totalRecebido > 1100.02) {
             vlInss = ((totalRecebido - 1100.01) * 0.09) + 82.50;
+            faixaInss = "2ª faixa (9%)";
         } 
         else if (totalRecebido < 1100.02) {
             vlInss = totalRecebido * 0.075;
-            if(totalRecebido <= 0) vlInss = 0;
-        } 
-                       
+            faixaInss = "1ª faixa (7,5%)";
+            if(totalRecebido <= 0) {
+                vlInss = 0;
+            }
+        }             
     }
+
     function calculaImpostoDeRenda(totalRecebido) {
         let total = totalRecebido - vlInss;
         if (total > 4664.68) {
-            vlIrrf = (total * 0.275) - 869.36;            
+            vlIrrf = (total * 0.275) - 869.36;
+            faixaIrrf = "Teto (27,5%)";            
         } 
         else if (total >= 3751.06) {
-            vlIrrf = (total * 0.225) - 636.13;            
+            vlIrrf = (total * 0.225) - 636.13;
+            faixaIrrf = "3ª faixa (22,5%)";            
         } 
         else if (total >= 2826.66) {
             vlIrrf = (total * 0.15) - 354.80;
+            faixaIrrf = "2ª faixa (15%)";
         } 
         else if (total >= 1903.99) {
             vlIrrf = (total * 0.075) - 142.80;
+            faixaIrrf = "1ª faixa (7,5%)";
         } 
         else if (total <= 1903.98) {
             vlIrrf = 0.0;
+            faixaIrrf = "Isento"
         }
-               
+    }
+
+    function calculaBruto(){
+        sBruto = (sBase + hrTotal) ;
+        
+          
+        
+    }
+    function calculaDescontos(){
+        totalDescontos = vlInss + vlIrrf;
     }
 
     function calculaPercent(){
-        percentInss = (100 / sBase) * vlInss;
-        percentIrrf = (100 / sBase) * vlIrrf;
-        percentLiquido = (100 / sBase) * sLiquido;
-        percentDescontos =  (100 / sBase) * (vlInss + vlIrrf) 
-        
-    }
+        percentInss = (100 / sBruto) * vlInss;
+        percentIrrf = (100 / sBruto) * vlIrrf;
+        percentLiquido = (100 / sBruto) * sLiquido;
+        percentDescontos =  (100 / sBruto) * (vlInss + vlIrrf);
+        fgtsMes = sBruto * 0.08;
 
+    }
     
     function executa() {
-        calculaInss(sBase);
-        calculaImpostoDeRenda(sBase);
+        tiraSobmraBotao();
+        calculaHora50();
+        calculaHora100();
+        calculaHoraSa();
+        calculaHorasTotal();
+        calculaBruto();
+        calculaInss(sBruto);
+        calculaImpostoDeRenda(sBruto);
+        calculaDescontos();
         calculaSalarioLiquido();
         calculaPercent();
-        document.getElementById('inss').innerHTML = "R$ " + vlInss.toFixed(2) + " (" + percentInss.toFixed(1) + " %)";
-        document.getElementById('irrf').innerHTML = "R$ " + vlIrrf.toFixed(2) + " (" + percentIrrf.toFixed(1) + " %)";
-        document.getElementById('bruto').innerHTML = "R$ " + sBase;
-        document.getElementById('liquido').innerHTML = "R$ " + sLiquido.toFixed(2)  + " (" + percentLiquido.toFixed(1) + " %)";
+        
+        
+        document.getElementById('inss').innerHTML = "R$ " + transformaBRL(vlInss.toFixed(2)) + " (" + percentInss.toFixed(1) + " %)";
+        document.getElementById('irrf').innerHTML = "R$ " + transformaBRL(vlIrrf.toFixed(2)) + " (" + percentIrrf.toFixed(1) + " %)";
+        document.getElementById('bruto').innerHTML = "R$ " + transformaBRL(sBruto.toFixed(2));
+        document.getElementById('liquido').innerHTML = "R$ " + transformaBRL(sLiquido.toFixed(2))  + " (" + percentLiquido.toFixed(1) + " %)";
+        document.getElementById('fInss').innerHTML = faixaInss;
+        document.getElementById('fIrff').innerHTML = faixaIrrf;
+        document.getElementById('fgtsMes').innerHTML = "R$ " + fgtsMes.toFixed(2);
+        document.getElementById('hrCinquenta').innerHTML = "R$ " + transformaBRL(hr50.toFixed(2));
+        document.getElementById('hrCem').innerHTML = "R$ " + transformaBRL(hr100.toFixed(2));
+        document.getElementById('hrSobreAviso').innerHTML = "R$ " + transformaBRL(hrsa.toFixed(2));
+        document.getElementById('heTotal').innerHTML ="R$ " + transformaBRL(hrTotal.toFixed(2));
+        document.getElementById('tDescontos').innerHTML ="R$ " + transformaBRL(totalDescontos.toFixed(2)) + " (" + percentDescontos.toFixed(1) + " %)"  ;
     }
